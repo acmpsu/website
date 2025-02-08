1aa801a658ca7307080c2747bbc12f54a919b32c
davidyoum <davidyoum@yahoo.com>
2025-02-08 15:34:41 -0500

    Maybe

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,83 +1,32 @@
 import Image from "next/image";
 import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "@/components/ui/carousel";
 
+const carouselItems = Array.from({ length: 5 }, (_, i) => ({
+    src: `/carousel-pictures/${i + 1}.jpg?w=800&q=75`,
+    alt: `ACM Event ${i + 1}`
+}));
+
 export default function CarouselComponent() {
     return (
-<Carousel className="w-full max-w-[800px]">
-              <CarouselContent className="h-[500px]">
-                <CarouselItem className="h-[500px]">
-
-                  <div className="relative w-full h-full">
-                    <Image
-                      src="/carousel-pictures/1.jpg"
-                      alt="ACM Event 1"
-                      className="object-cover rounded-lg w-full h-full"
-                      width={800}
-                      height={500}
-                      priority
-                        unoptimized
-                    />
-                  </div>
-                </CarouselItem>
-
-                <CarouselItem className="h-[500px]">
-                  <div className="relative w-full h-full">
-                    <Image
-                      src="/carousel-pictures/2.jpg"
-                      alt="ACM Event 2"
-                      className="object-cover rounded-lg w-full h-full"
-                      width={800}
-                      height={500}  
-                      priority
-                        unoptimized
-                    />
-                  </div>
-                </CarouselItem>
-
-                <CarouselItem className="h-[500px]">
-                  <div className="relative w-full h-full">
-                    <Image
-                      src="/carousel-pictures/3.jpg"
-                      alt="ACM Event 3"
-                      className="object-cover rounded-lg w-full h-full"
-                      width={800}
-                      height={500}
-                      priority
-                        unoptimized
-                    />
-                  </div>
-                </CarouselItem>
-
-                <CarouselItem className="h-[500px]">
-                  <div className="relative w-full h-full">
-                    <Image
-                      src="/carousel-pictures/4.jpg"
-                      alt="ACM Event 4"
-                      className="object-cover rounded-lg w-full h-full"
-                      width={800}
-                      height={500}
-                      priority
-                        unoptimized
-                    />
-                  </div>
-                </CarouselItem>
-
-                <CarouselItem className="h-[500px]">
-                  <div className="relative w-full h-full">
-                    <Image
-                      src="/carousel-pictures/5.jpg"
-                      alt="ACM Event 5"
-                      className="object-cover rounded-lg w-full h-full"
-                      width={800}
-                      height={500}
-                      priority
-                        unoptimized
-                    />
-                  </div>
-                </CarouselItem>
-              </CarouselContent>
-              <CarouselPrevious className="hidden sm:flex" />
-              <CarouselNext className="hidden sm:flex" />
-            </Carousel>
+        <Carousel className="w-full max-w-[800px]">
+            <CarouselContent className="h-[500px]">
+                {carouselItems.map((item, index) => (
+                    <CarouselItem key={index} className="h-[500px]">
+                        <div className="relative w-full h-full">
+                            <Image
+                                src={item.src}
+                                alt={item.alt}
+                                className="object-cover rounded-lg w-full h-full"
+                                width={800}
+                                height={500}
+                                priority={index === 0}
+                            />
+                        </div>
+                    </CarouselItem>
+                ))}
+            </CarouselContent>
+            <CarouselPrevious className="hidden sm:flex" />
+            <CarouselNext className="hidden sm:flex" />
+        </Carousel>
     )
 }
