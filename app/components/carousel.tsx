6a494cf7f03fa9b4f63abe3ae345c3dcf8611b70
davidyoum <davidyoum@yahoo.com>
2025-02-08 15:55:42 -0500

    maybe

--- app/components/carousel.tsx
+++ app/components/carousel.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from "next/image";
 import { Carousel, CarouselContent, CarouselItem, CarouselPrevious, CarouselNext } from "@/components/ui/carousel";
 
@@ -8,25 +10,27 @@ const carouselItems = Array.from({ length: 5 }, (_, i) => ({
 
 export default function CarouselComponent() {
     return (
-        <Carousel className="w-full max-w-[800px]">
-            <CarouselContent className="h-[500px]">
-                {carouselItems.map((item, index) => (
-                    <CarouselItem key={index} className="h-[500px]">
-                        <div className="relative w-full h-full">
-                            <Image
-                                src={item.src}
-                                alt={item.alt}
-                                className="object-cover rounded-lg w-full h-full"
-                                width={800}
-                                height={500}
-                                priority={index === 0}
-                            />
-                        </div>
-                    </CarouselItem>
-                ))}
-            </CarouselContent>
-            <CarouselPrevious className="hidden sm:flex" />
-            <CarouselNext className="hidden sm:flex" />
-        </Carousel>
-    )
+        <div className="flex items-center justify-center h-[500px] w-full">
+            <Carousel className="w-full max-w-[800px]">
+                <CarouselContent className="h-[500px]">
+                    {carouselItems.map((item, index) => (
+                        <CarouselItem key={index} className="h-[500px]">
+                            <div className="relative w-full h-full">
+                                <Image
+                                    src={item.src}
+                                    alt={item.alt}
+                                    className="object-cover rounded-lg"
+                                    fill
+                                    sizes="(max-width: 800px) 100vw, 800px"
+                                    priority={index === 0}
+                                />
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious className="hidden sm:flex" />
+                <CarouselNext className="hidden sm:flex" />
+            </Carousel>
+        </div>
+    );
 }
